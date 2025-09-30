5973bc5355fc2a180b73b54cdd2a82448e62a837
qkrwotjd1731 <58104546+qkrwotjd1731@users.noreply.github.com>
2025-09-30 16:29:37 +0900

    feat: add hi function in hello.ts

--- src/hello.ts
+++ src/hello.ts
@@ -2,6 +2,9 @@ class HelloController {
   hello(name: string) {
     return `👋🏻 Hello, ${name}!`;
   }
+  hi(name: string) {
+    return `🙌🏻 Hi, ${name}!`;
+  }
 }
 
 export default new HelloController();
